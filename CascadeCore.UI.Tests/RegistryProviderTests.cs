c5ba22a6268fcf053ad21b1fc05c897f1755f416
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-06-18 22:07:47 -0700

    Update registry provider

---
+++ CascadeCore.UI.Tests/RegistryProviderTests.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CascadePass.CascadeCore.UI.Tests
+{
+    [TestClass]
+    public class RegistryProviderTests
+    {
+        #region GetValue Tests
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("\r")]
+        [DataRow("\n")]
+        [DataRow("\t")]
+        [DataRow("\r\n")]
+        public void GetValue_ThrowsForEmptyKeyName(string verionOfEmpty)
+        {
+            var registryProvider = new RegistryProvider();
+            Assert.ThrowsException<ArgumentException>(() => registryProvider.GetValue(verionOfEmpty, "AnyValue"));
+        }
+
+        [TestMethod]
+        public void GetValue_ReturnsNullAndRaisesEvent_WhenKeyDoesNotExist()
+        {
+            var registryProvider = new RegistryProvider();
+            string keyName = "NonExistentKey";
+            string valueName = "NonExistentValue";
+
+            var result = registryProvider.GetValue(keyName, valueName);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GetValue_RaisesRegistryAccessedEvent()
+        {
+            var registryProvider = new RegistryProvider();
+            string keyName = "DoesNotMatter";
+            string valueName = "DoesNotMatter";
+            RegistryAccessEventArgs receivedArgs = null;
+
+            registryProvider.RegistryAccessed += (sender, args) =>
+            {
+                receivedArgs = args;
+            };
+
+            var result = registryProvider.GetValue(keyName, valueName);
+
+            Assert.IsNull(result);
+            Assert.IsNotNull(receivedArgs);
+            Assert.AreEqual(RegistryAccessType.Read, receivedArgs.AccessType);
+            Assert.AreEqual(keyName, receivedArgs.KeyName);
+            Assert.AreEqual(valueName, receivedArgs.ValueName);
+        }
+
+        [TestMethod]
+        public void GetValue_RaisesRegistryAccessedEvent_OnlyOnce()
+        {
+            var registryProvider = new RegistryProvider();
+            string keyName = "ArbitraryKey";
+            string valueName = "ArbitraryValue";
+            int eventCount = 0;
+
+            registryProvider.RegistryAccessed += (sender, args) =>
+            {
+                eventCount++;
+            };
+
+            var result = registryProvider.GetValue(keyName, valueName);
+
+            Assert.IsNull(result);
+            Assert.AreEqual(1, eventCount, "Expected only one RegistryAccessed event to be raised.");
+        }
+
+        [TestMethod]
+        public async Task GetValue_RaisesRegistryAccessedAsyncEvent()
+        {
+            var registryProvider = new RegistryProvider();
+            string keyName = "AsyncKey";
+            string valueName = "AsyncValue";
+
+            RegistryAccessEventArgs receivedArgs = null;
+            var tcs = new TaskCompletionSource<bool>();
+
+            registryProvider.RegistryAccessedAsync += async (sender, args) =>
+            {
+                receivedArgs = args;
+                tcs.SetResult(true);
+                await Task.CompletedTask;
+            };
+
+            var result = registryProvider.GetValue(keyName, valueName);
+
+            await Task.WhenAny(tcs.Task, Task.Delay(500)); // Wait for async event or timeout
+
+            Assert.IsNull(result);
+            Assert.IsTrue(tcs.Task.IsCompleted, "Async event did not complete in time.");
+            Assert.IsNotNull(receivedArgs);
+            Assert.AreEqual(RegistryAccessType.Read, receivedArgs.AccessType);
+            Assert.AreEqual(keyName, receivedArgs.KeyName);
+            Assert.AreEqual(valueName, receivedArgs.ValueName);
+        }
+
+        [TestMethod]
+        public async Task GetValue_RaisesRegistryAccessedAsyncEvent_OnlyOnce()
+        {
+            var registryProvider = new RegistryProvider();
+            string keyName = "AsyncKey_Single";
+            string valueName = "AsyncValue_Single";
+
+            int asyncCount = 0;
+            var tcs = new TaskCompletionSource<bool>();
+
+            registryProvider.RegistryAccessedAsync += async (sender, args) =>
+            {
+                Interlocked.Increment(ref asyncCount);
+                tcs.SetResult(true);
+                await Task.CompletedTask;
+            };
+
+            var result = registryProvider.GetValue(keyName, valueName);
+
+            await Task.WhenAny(tcs.Task, Task.Delay(500));
+
+            Assert.IsNull(result);
+            Assert.IsTrue(tcs.Task.IsCompleted, "Async event did not fire.");
+            Assert.AreEqual(1, asyncCount, "Expected only one async event invocation.");
+        }
+
+        #endregion
+
+        #region SetValue Tests
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow("\r")]
+        [DataRow("\n")]
+        [DataRow("\t")]
+        [DataRow("\r\n")]
+        public void SetValue_ThrowsForEmptyKeyName(string verionOfEmpty)
+        {
+            var registryProvider = new RegistryProvider();
+            Assert.ThrowsException<ArgumentException>(() => registryProvider.SetValue(verionOfEmpty, "AnyValueName", "ValueTheCallerWantsToSaveToTheRegistry", RegistryValueKind.String));
+        }
+
+
+        [TestMethod]
+        public void SetValue_ReturnsNullAndRaisesEvent_WhenKeyDoesNotExist()
+        {
+            var registryProvider = new RegistryProvider();
+            string keyName = "NonExistentKey";
+            string valueName = "NonExistentValue";
+            string valueToSet = "ValueTheCallerWantsToSaveToTheRegistry";
+
+            var result = registryProvider.SetValue(keyName, valueName, valueToSet, RegistryValueKind.String);
+            registryProvider.DeleteValue(keyName, valueName);
+
+            Assert.IsTrue(result);
+        }
+
+        #endregion
+    }
+}
--- CascadeCore.UI/RegistryProvider.cs
+++ CascadeCore.UI/RegistryProvider.cs
@@ -1,15 +1,238 @@
 ﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CascadePass.CascadeCore.UI
 {
+
     public class RegistryProvider : IRegistryProvider
     {
+        private readonly RegistryHive hive;
+
+        public delegate void RegistryAccessedHandler(object sender, RegistryAccessEventArgs e);
+        public delegate Task RegistryAccessedAsyncHandler(object sender, RegistryAccessEventArgs e);
+
+        public event RegistryAccessedHandler RegistryAccessed;
+        public event RegistryAccessedAsyncHandler RegistryAccessedAsync;
+
+        public RegistryProvider()
+        {
+            this.hive = RegistryHive.CurrentUser;
+        }
+
+        public RegistryProvider(RegistryHive targetHive)
+        {
+            this.hive = targetHive;
+        }
+
+        private RegistryKey OpenKey(string keyName, bool writable)
+        {
+            if (string.IsNullOrWhiteSpace(keyName))
+            {
+                throw new ArgumentException(null, nameof(keyName));
+            }
+
+            return RegistryKey.OpenBaseKey(hive, RegistryView.Default).OpenSubKey(keyName, writable);
+        }
+
         public object GetValue(string keyName, string valueName)
         {
-            using RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName);
+            if (string.IsNullOrWhiteSpace(keyName))
+            {
+                throw new ArgumentException(null, nameof(keyName));
+            }
 
-            key?.Flush();
-            return key?.GetValue(valueName);
+            try
+            {
+                using var key = this.OpenKey(keyName, writable: false);
+                object value = key?.GetValue(valueName);
+
+                this.OnRegistryAccessed(new(this.hive, keyName, valueName, RegistryAccessType.Read));
+                return value;
+            }
+            catch (Exception ex)
+            {
+                this.OnRegistryAccessed(new(this.hive, keyName, valueName, RegistryAccessType.Read, ex));
+                return null;
+            }
         }
+
+        public bool SetValue(string keyName, string valueName, object value, RegistryValueKind valueKind)
+        {
+            if (string.IsNullOrWhiteSpace(keyName))
+            {
+                throw new ArgumentException(null, nameof(keyName));
+            }
+
+            try
+            {
+                using var key = RegistryKey.OpenBaseKey(hive, RegistryView.Default).CreateSubKey(keyName);
+                key?.SetValue(valueName, value, valueKind);
+
+                this.OnRegistryAccessed(new(this.hive, keyName, valueName, RegistryAccessType.Write));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.OnRegistryAccessed(new(this.hive, keyName, valueName, RegistryAccessType.Write, ex));
+                return false;
+            }
+        }
+
+        public bool DeleteValue(string keyName, string valueName)
+        {
+            if (string.IsNullOrWhiteSpace(keyName))
+            {
+                throw new ArgumentException(null, nameof(keyName));
+            }
+
+            try
+            {
+                using var key = this.OpenKey(keyName, writable: true);
+                key?.DeleteValue(valueName, throwOnMissingValue: false);
+
+                this.OnRegistryAccessed(new(this.hive, keyName, valueName, RegistryAccessType.Delete));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                this.OnRegistryAccessed(new(this.hive, keyName, valueName, RegistryAccessType.Delete, ex));
+                return false;
+            }
+        }
+
+        public string[] GetSubKeyNames(string keyName)
+        {
+            if (string.IsNullOrWhiteSpace(keyName))
+            {
+                throw new ArgumentException(null, nameof(keyName));
+            }
+
+            try
+            {
+                using var key = this.OpenKey(keyName, writable: false);
+                var result = key?.GetSubKeyNames() ?? [];
+
+                this.OnRegistryAccessed(new(this.hive, keyName, RegistryAccessType.EnumerateKeys));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                this.OnRegistryAccessed(new(this.hive, keyName, RegistryAccessType.EnumerateKeys, ex));
+                return null;
+            }
+        }
+
+        public string[] GetValueNames(string keyName)
+        {
+            if (string.IsNullOrWhiteSpace(keyName))
+            {
+                throw new ArgumentException(null, nameof(keyName));
+            }
+
+            try
+            {
+                using var key = this.OpenKey(keyName, writable: false);
+                var result = key?.GetValueNames() ?? [];
+
+                this.OnRegistryAccessed(new(this.hive, keyName, RegistryAccessType.EnumerateValues));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                this.OnRegistryAccessed(new(this.hive, keyName, RegistryAccessType.EnumerateValues, ex));
+                return null;
+            }
+        }
+
+
+
+
+        protected virtual void OnRegistryAccessed(RegistryAccessEventArgs e)
+        {
+            this.RegistryAccessed?.Invoke(this, e);
+
+            if (this.RegistryAccessedAsync != null)
+            {
+                var invocationList = this.RegistryAccessedAsync.GetInvocationList().Cast<RegistryAccessedAsyncHandler>();
+                foreach (var handler in invocationList)
+                {
+                    _ = handler(this, e); // Fire-and-forget async
+                }
+            }
+        }
+    }
+
+    public class RegistryAccessEventArgs : EventArgs
+    {
+        public RegistryAccessEventArgs(
+            RegistryHive hive,
+            string keyName,
+            RegistryAccessType accessType)
+        {
+            this.KeyName = keyName;
+            this.AccessType = accessType;
+        }
+
+        public RegistryAccessEventArgs(
+            RegistryHive hive,
+            string keyName,
+            string valueName,
+            RegistryAccessType accessType)
+        {
+            this.Hive = hive;
+            this.KeyName = keyName;
+            this.ValueName = valueName;
+            this.AccessType = accessType;
+        }
+
+        public RegistryAccessEventArgs(
+            RegistryHive hive,
+            string keyName,
+            RegistryAccessType accessType,
+            Exception exception)
+        {
+            this.Hive = hive;
+            this.KeyName = keyName;
+            this.AccessType = accessType;
+            this.Exception = exception;
+        }
+
+        public RegistryAccessEventArgs(
+            RegistryHive hive,
+            string keyName,
+            string valueName,
+            RegistryAccessType accessType,
+            Exception exception)
+        {
+            this.Hive = hive;
+            this.KeyName = keyName;
+            this.ValueName = valueName;
+            this.AccessType = accessType;
+            this.Exception = exception;
+        }
+
+        public RegistryHive Hive { get; }
+        public string KeyName { get; }
+        public string ValueName { get; }
+        public RegistryAccessType AccessType { get; }
+        public bool WasSuccessful => this.Exception == null;
+        public Exception Exception { get; }
+    }
+
+    public enum RegistryAccessType
+    {
+        None,
+
+        Read,
+
+        Write,
+
+        Delete,
+
+        EnumerateKeys,
+
+        EnumerateValues,
     }
 }
