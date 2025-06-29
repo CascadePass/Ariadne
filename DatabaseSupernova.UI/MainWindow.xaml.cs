1c4d14a1a396b9c5b6415b96f436da2c52e3b470
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-06-29 11:52:03 -0700

    Custom title bar

--- DatabaseSupernova.UI/MainWindow.xaml.cs
+++ DatabaseSupernova.UI/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace CascadePass.DatabaseSupernova
 {
@@ -7,9 +12,32 @@ namespace CascadePass.DatabaseSupernova
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int DWMWA_WINDOW_CORNER_PREFERENCE = 33;
+        const int DWMWCP_ROUND = 2;
+
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
         public MainWindow()
         {
             this.InitializeComponent();
+            this.Loaded += (_, __) => ApplyRoundedCorners();
+        }
+
+        private void ApplyRoundedCorners()
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            int preference = DWMWCP_ROUND;
+
+            DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, ref preference, sizeof(int));
+        }
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
     }
 }
---
+++ DatabaseSupernova.UI/WorkspaceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CascadePass.CascadeCore.UI;
+
+namespace CascadePass.DatabaseSupernova.UI
+{
+    public class WorkspaceViewModel : ViewModel
+    {
+        private bool isSearchVisible;
+
+        public bool IsSearchVisible
+        {
+            get => isSearchVisible;
+            set => this.SetPropertyValue(ref isSearchVisible, value, nameof(this.IsSearchVisible));
+        }
+    }
+}
