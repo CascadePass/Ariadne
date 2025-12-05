076595d75ee3e129f8fa2df494d096bf44ff8c82
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-12-04 17:52:33 -0800

    Add RectConverter for WPF
    Add converter that builds a rectangle the height and width of its two parameters.

---
+++ CascadePass.Core.UI/Converters/RectConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CascadePass.Core.UI.Converters
+{
+    public class RectConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is double width && values[1] is double height)
+            {
+                return new Rect(0, 0, width, height);
+            }
+            return Rect.Empty;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
