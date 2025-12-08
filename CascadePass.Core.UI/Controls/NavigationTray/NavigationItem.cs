c7084e78527818dc7512e5f9a6d8e357bde25ce7
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-12-07 16:30:33 -0800

    Improve Navigation Tray
    NavigationTray Improvements:
    
    * Selection Stripe is now a dependency property
    * Better INotifyPropertyChanged support in NavigationItem
    * Add IsVisible property to NavigationItem
    * Add ToString override to NavigationItem
    * Add FontWeight property to NavigationItem
    * Add Foreground, Background, MouseOver, Pressed properties to NavigationItem
    * Add Tag property to NavigationItem
    * Removed NavigationCommand
    * Add NavToTrayBrushConverter, SelectionAwareBrushConverter, SelectionAwareFontWeightConverter
    * Add ItemForeground property to NavigationTray
    * Add SelectedForeground, SelectedBackground, and SelectedFontWeight to NavigationTray
    * Add ItemCornerRadius to NavigationTray and NavigationButton
    * Add OpenedWidth, ClosedWidth, and OpenCloseDuration properties (TimeSpan.Zero = no animation)

--- CascadePass.Core.UI/Controls/NavigationTray/NavigationItem.cs
+++ CascadePass.Core.UI/Controls/NavigationTray/NavigationItem.cs
@@ -1,12 +1,30 @@
-﻿using System.Windows.Input;
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace CascadePass.Core.UI.Controls
 {
-    public class NavigationItem : ViewModel
+    public class NavigationItem : Observable
     {
+        #region Private fields
+
         private string textLabel;
-        private bool isSelected, showSelectedIcon;
-        private object tooltip;
+        private string iconUri, selectedIconUri;
+        private bool isSelected, showSelectedIcon, isVisible;
+        private object tooltip, tag, commandParameter;
+        private ICommand command;
+
+        private FontWeight fontWeight;
+        private Brush foreground, background, mouseOver, pressed;
+        private CornerRadius cornerRadius;
+
+        #endregion
+
+        public NavigationItem()
+        {
+            this.isVisible = true;
+            this.fontWeight = FontWeights.Normal;
+        }
 
         public string Label
         {
@@ -14,19 +32,34 @@ namespace CascadePass.Core.UI.Controls
             set => this.SetPropertyValue(ref this.textLabel, value, nameof(this.Label));
         }
 
+        public string Icon {
+            get => this.iconUri;
+            set => this.SetPropertyValue(ref this.iconUri, value, [nameof(this.Icon), nameof(this.EffectiveSelectedIcon), nameof(this.IconPath),]);
+        }
 
-        public string Icon { get; set; }
-        public string SelectedIcon { get; set; }
+        public string SelectedIcon {
+            get => this.selectedIconUri;
+            set => this.SetPropertyValue(ref this.selectedIconUri, value, [nameof(this.SelectedIcon), nameof(this.EffectiveSelectedIcon), nameof(this.IconPath),]);
+        }
+
+        public string EffectiveSelectedIcon => string.IsNullOrEmpty(this.SelectedIcon) ? this.Icon : this.SelectedIcon;
 
         public object ToolTip {
             get => this.tooltip;
             set => this.SetPropertyValue(ref this.tooltip, value, nameof(this.ToolTip));
         }
 
-        public ICommand Command { get; set; }
-        public object CommandParameter { get; set; }
+        public ICommand Command
+        {
+            get => this.command;
+            set => this.SetPropertyValue(ref this.command, value, nameof(this.Command));
+        }
 
-        public ICommand NavigationCommand { get; set; }
+        public object CommandParameter
+        {
+            get => this.commandParameter;
+            set => this.SetPropertyValue(ref this.commandParameter, value, nameof(this.CommandParameter));
+        }
 
         public bool IsSelected
         {
@@ -41,7 +74,98 @@ namespace CascadePass.Core.UI.Controls
         }
 
         public string IconPath => this.IsSelected || this.ShowSelectedIcon
-                ? this.SelectedIcon
+                ? this.EffectiveSelectedIcon
                 : this.Icon;
+
+        public bool IsVisible
+        {
+            get => this.isVisible;
+            set => this.SetPropertyValue(ref this.isVisible, value, nameof(this.IsVisible));
+        }
+
+        public FontWeight FontWeight
+        {
+            get => this.fontWeight;
+            set => this.SetPropertyValue(ref this.fontWeight, value, nameof(this.FontWeight));
+        }
+
+        public Brush Foreground
+        {
+            get => this.foreground;
+            set => this.SetPropertyValue(ref this.foreground, value, nameof(this.Foreground));
+        }
+
+        public Brush Background
+        {
+            get => this.background;
+            set => this.SetPropertyValue(ref this.background, value, nameof(this.Background));
+        }
+
+        public Brush MouseOver
+        {
+            get => this.mouseOver;
+            set => this.SetPropertyValue(ref this.mouseOver, value, nameof(this.MouseOver));
+        }
+
+        public Brush Pressed
+        {
+            get => this.pressed;
+            set => this.SetPropertyValue(ref this.pressed, value, nameof(this.Pressed));
+        }
+
+        /// <summary>
+        /// Arbitrary metadata for the navigation item.
+        /// Typically used to store the destination view type.
+        /// </summary>
+        public object Tag
+        {
+            get => this.tag;
+            set => this.SetPropertyValue(ref this.tag, value, nameof(this.Tag));
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get => this.cornerRadius;
+            set => this.SetPropertyValue(ref this.cornerRadius, value, nameof(this.CornerRadius));
+        }
+
+        public override string ToString()
+        {
+            return this.Label ?? base.ToString();
+        }
+
+        #region Static Creation Helpers
+
+        public static NavigationItem Create(string label, string iconUrl)
+        {
+            return new NavigationItem()
+            {
+                Label = label,
+                Icon = iconUrl,
+            };
+        }
+
+        public static NavigationItem Create(string label, string iconUrl, string selectedIconUri)
+        {
+            return new NavigationItem()
+            {
+                Label = label,
+                Icon = iconUrl,
+                SelectedIcon = selectedIconUri,
+            };
+        }
+
+        public static NavigationItem Create(string label, string iconUrl, string selectedIconUri, object toolTip)
+        {
+            return new NavigationItem()
+            {
+                Label = label,
+                Icon = iconUrl,
+                SelectedIcon = selectedIconUri,
+                ToolTip = toolTip,
+            };
+        }
+
+        #endregion
     }
 }
---
+++ CascadePass.Core.UI/Converters/NullToTrayBrushConverter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CascadePass.Core.UI.Converters
+{
+    public class NullToTrayBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var itemBrush = values[0] as Brush;
+            var trayBrush = values[1] as Brush;
+            return itemBrush ?? trayBrush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class SelectionAwareBrushConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = values[0] is bool b && b;
+            var itemBrush = values[1] as Brush;
+            var trayBrush = values[2] as Brush;
+            var selectedBrush = values[3] as Brush;
+
+            if (isSelected)
+                return selectedBrush ?? itemBrush ?? trayBrush;
+            return itemBrush ?? trayBrush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class SelectionAwareFontWeightConverter : IMultiValueConverter
+    {
+        private static FontWeight? GetFontWeight(object value)
+        {
+            if (value == null || value == DependencyProperty.UnsetValue)
+                return null;
+
+            return value is FontWeight fw ? fw : (FontWeight?)null;
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = values[0] is bool b && b;
+            FontWeight? item = GetFontWeight(values[1]);          // NavigationButton.Item FontWeight (from NavigationItem)
+            FontWeight? trayDefault = GetFontWeight(values[2]);   // NavigationTray.Item default FontWeight (optional)
+            FontWeight? traySelected = GetFontWeight(values[3]);  // NavigationTray.SelectedFontWeight
+
+            if (isSelected)
+                return traySelected ?? item ?? trayDefault ?? FontWeights.Normal;
+
+            return item ?? trayDefault ?? FontWeights.Normal;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class SelectionAwareCornerRadiusConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = values[0] is bool b && b;
+            CornerRadius itemOverride = values[1] as CornerRadius? ?? new CornerRadius(0);
+            CornerRadius trayDefault = values[2] as CornerRadius? ?? new CornerRadius(0);
+            CornerRadius traySelected = values[3] as CornerRadius? ?? new CornerRadius(0);
+
+            if (isSelected)
+            {
+                // Prefer item override if set, otherwise tray selected
+                return !IsZero(itemOverride) ? itemOverride : traySelected;
+            }
+            else
+            {
+                // Prefer item override if set, otherwise tray default
+                return !IsZero(itemOverride) ? itemOverride : trayDefault;
+            }
+        }
+
+        private bool IsZero(CornerRadius cr) =>
+            cr.TopLeft == 0 && cr.TopRight == 0 && cr.BottomRight == 0 && cr.BottomLeft == 0;
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+}
