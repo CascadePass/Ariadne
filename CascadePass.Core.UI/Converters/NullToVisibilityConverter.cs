9b7bfaf69e53824915dec0017d71f93f25852d25
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-12-21 22:03:38 -0800

    Add NullToVisibilityConverter
    Add converter to take any object and return collapsed or visible based on whether it's null.

---
+++ CascadePass.Core.UI/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CascadePass.Core.UI.Converters
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+            {
+                return Visibility.Collapsed;
+            }
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
